a9a50978036b413f43dd19ac407e69e47e8e14b0
imogutov <imogutov@me.com>
2023-01-18 21:50:37 +0300

    Выполнено ДЗ MapKit

--- Navigation/Factory/Factory.swift
+++ Navigation/Factory/Factory.swift
@@ -5,6 +5,7 @@ final class Factory {
     enum Flow {
         case feedVC
         case loginVC
+        case mapVC
     }
     
     let navigationController: UINavigationController
@@ -29,6 +30,10 @@ final class Factory {
             loginViewController.delegate = loginInspectorFactory.createLoginInspector()
             navigationController.tabBarItem = UITabBarItem(title: "login", image: UIImage(systemName: "house.fill"), tag: 1)
             navigationController.setViewControllers([loginViewController], animated: true)
+        case .mapVC:
+            let mapViewController = MapViewController()
+            navigationController.tabBarItem = UITabBarItem(title: "map", image: UIImage(systemName: "map.fill"), tag: 2)
+            navigationController.setViewControllers([mapViewController], animated: true)
         }
     }
 }
